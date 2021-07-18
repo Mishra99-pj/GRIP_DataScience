abebb8824823da7f28ccaa2f42691693ff7b9d8e
Mishra99-pj <86367226+Mishra99-pj@users.noreply.github.com>
2021-07-19 02:56:46 +0530

    Create LinearRegression.py
    This linearRegression.py file contains the codes to create a linear regression model based on some data which contain information about a student's study duration(in hrs per day) in relation with the marks stored by the student.
    
    At the end the model also predicts for a student whose study duration is 10.05 hr/day.

---
+++ LinearRegression.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+data = pd.read_csv(r"http://bit.ly/w-data")
+
+ip = data.drop(['Scores'], axis=1)
+op = data['Scores']
+
+from sklearn.model_selection import train_test_split
+Xtr,Xts,Ytr,Yts=train_test_split(ip,op,test_size=0.2)
+
+
+from sklearn.linear_model import LinearRegression
+alg=LinearRegression()
+alg.fit(Xtr,Ytr)
+m=alg.coef_
+c=alg.intercept_
+print("m is:",m)
+print("c is:",c)
+
+import matplotlib.pyplot as plt
+plt.scatter(ip,op,c="blue")
+y=m*ip+c
+plt.plot(ip,y,c="r")
+plt.show()
+
+print("Accuracy:",alg.score(Xts,Yts))
+
+import numpy as np
+test=np.array([10.05]).reshape(1,-1)
+print("Prediction:",alg.predict(test))
